c04fed2a340a8bd1ba7e0642a5ab87ad91f6fae2
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-07-17 23:29:20 +0300

    Add style files

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
+import "./styles/global.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
